8707144de8c022877ab6f47a94da2d3741d01b42
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2019-03-18 17:21:18 +0000

    CSS change

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ UPLOAD_FOLDER = './app/static/profile_pics'
 
 ALLOWED_EXTS =set(['png','jpg'])
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://profile:profilepsd@localhost/profile"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://tifmbqnuadlsfc:46b554314cff72d0a57fe39c7bcd101ad5035aeb8ccd68b10515bff203b681eb@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d81mmd336gnaht"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
